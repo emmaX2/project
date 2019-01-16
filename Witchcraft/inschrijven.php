162e76a7748c352f2d28e6ceaf41d40be10a7ab8
TOAD163 <toad@toadcraft.net>
2019-01-16 11:33:16 +0100

    inschrijf pagina toegevoegd

--- Witchcraft/inschrijven.php
+++ Witchcraft/inschrijven.php
@@ -1,91 +1,119 @@
 <!doctype html>
-<html lang="nl">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Oswald:500" rel="stylesheet">
-    <title>Inschrijven</title>
-  </head>
-  <body>
-      <div class="container">
-        <ul class="nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="opleiding.html">Opleiding</a>
-                </li>
-                <li class="nav-item">
-                        <a class="nav-link" href="praktisch.html">Praktisch</a>
-                    </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="inschrijven.html">Inschrijven</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="game.html">Game</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Contact</a>
-                </li>
-            </ul>
-            <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
+<html lang="nl">
+
+<head>
+
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<link rel="stylesheet" href="./css/style.css">
+
+
+
+<title>Hello, world!</title>
+
+</head>
+
+<body>
+
+<main class="container">
+
+<div class="row">
+
+<div class="col-12">
+
+<div class="jumbotron jumbotron-fluid">
+
+<div class="container">
+
+<h1 class="display-4">Inschrijfformulier</h1>
+
+<p class="lead">Schrijf je hier in voor de opleiding witchcraft.</p>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<div class="row">
+
+<div class="col-6">
+
+<form action="./create.php" method="post">
+
+<div class="form-group">
+
+<label for="voornaam">Voornaam</label>
+
+<input type="text" class="form-control" id="voornaam" aria-describedby="voornaamHelp" placeholder="Voornaam" name="firstname">
+
+</div>
+
+<div class="form-group">
+
+<label for="Tussenvoegsel">Tussenvoegsel</label>
+
+<input type="text" class="form-control" id="Tussenvoegsel" aria-describedby="TussenvoegselHelp" placeholder="Tussenvoegsel" name="infix">
+
+</div>
+
+<div class="form-group">
+
+<label for="Achternaam">Achternaam</label>
+
+<input type="text" class="form-control" id="Achternaam" aria-describedby="AchternaamHelp" placeholder="Achternaam" name="lastname">
+
+<small id="AchternaamHelp" class="form-text text-muted">We zullen deze informatie alleen met MBO Utrecht delen</small>
+
+</div>
+
+<button type="submit" class="btn btn-primary">Stuur het op</button>
+
 </form>
 
-            
 </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+
+
+</div>
+<div class="row">
+<div class="col-12">
+<button type="button" class ="btn btn-primary btn-lg btn-block">nieuw record</button>
+</div>
+<div class="row">
+
+<div class="col-12"><br>
+
+<p>
+
+</p>
+
+</div>
+
+</div>
+
+
+
+</main>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<script src="./js/app.js"></script>
+
+</body>
+
 </html>
+
